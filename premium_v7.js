6b6a1c79c18c062d53f90edd931c6bd966483ed1
AlbertSJG <albertosjg@gmail.com>
2026-04-14 21:10:21 +0100

    fix(assets): migrate generated logos to official brand pngs from 02_branding folder

---
+++ premium_v7.js
@@ -0,0 +1,10 @@
+﻿const fs = require('fs');
+let html = fs.readFileSync('../frontend/index.html', 'utf8');
+
+// Quitar mi SVG Cerebro Inventado por la imagen real brandeada
+let gloriaSVGRegex = /<svg class="gloria-brain-avatar" xmlns="http:\/\/www.w3.org\/2000\/svg"[\s\S]*?<\/svg>/;
+let newGloriaIMG = `<img class="gloria-brain-avatar" src="img/logo-gloria.png" alt="Cerebro de GlorIA" style="width: 180px; height: auto; filter: drop-shadow(0 0 25px rgba(138, 75, 222, 0.4)); animation: float-brain 6s ease-in-out infinite;">`;
+html = html.replace(gloriaSVGRegex, newGloriaIMG);
+
+fs.writeFileSync('../frontend/index.html', html);
+fs.writeFileSync('./03_landing/index.html', html);
