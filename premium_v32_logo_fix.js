b17b376591128f7759ceeede1c148b9a207c57dc
AlbertSJG <albertosjg@gmail.com>
2026-04-19 21:26:19 +0100

    fix(ui): restore missing header logo and normalize source path

---
+++ premium_v32_logo_fix.js
@@ -0,0 +1,17 @@
+﻿const fs = require('fs');
+
+function fixLogoSource(filepath) {
+    if (!fs.existsSync(filepath)) return;
+    let html = fs.readFileSync(filepath, 'utf8');
+
+    // Nos aseguramos de que el src apunte a logo-rentos.png que es el archivo que acabamos de copiar
+    // Buscamos cualquier referencia al logo en la cabecera y la normalizamos
+    html = html.replace(/src="img\/logo_RentOS\.png"/g, 'src="img/logo-rentos.png"');
+    html = html.replace(/src="img\/logo\.png"/g, 'src="img/logo-rentos.png"');
+
+    fs.writeFileSync(filepath, html);
+    console.log('Ruta del logo normalizada en ' + filepath);
+}
+
+fixLogoSource('../frontend/index.html');
+fixLogoSource('./03_landing/index.html');
